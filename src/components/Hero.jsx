a93f4030247e01b0713ee63e47e5544ea749bccb
SyedAteef1 <vsyedateefquadri@gmail.com>
2024-12-31 20:46:56 +0530

    Description of your changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import Nav from './Nav'
 export default function Hero(){
     return (
-        <section className=" container bg-[radial-gradient(ellipse_300%_100%_at_bottom_center,#4DA1A9,white_120%)] h-[100vh]">
+        <section className=" sad 1w bg-[radial-gradient(ellipse_300%_100%_at_bottom_center,#4DA1A9,white_120%)] h-[100vh]">
             <Nav></Nav>
             <div>
                 <h1 className="text-black">this is hero section</h1>
