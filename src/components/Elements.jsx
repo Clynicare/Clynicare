6dfbf7a16bf56c2684159fe12ed538af89e5ccc4
git@1711 <mohammedsohail5646@gmail.com>
2025-02-02 13:14:09 +0530

    final changes

--- src/components/Elements.jsx
+++ src/components/Elements.jsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import React from 'react';
 
-const Elements = () => {
+export default function Elements  (){
   return (
     <div className="elements relative">
       {/* <Image src="/images/plus.png" width={1000} height={1000} alt="Plus Icon" className='absolute flex inset-y-0 right-0'/> */}
@@ -9,4 +9,4 @@ const Elements = () => {
   );
 };
 
-export default Elements;
+ 
