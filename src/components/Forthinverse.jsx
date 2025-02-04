6bd8f71b88d5624d793de62f4521994496243fe0
git@1711 <mohammedsohail5646@gmail.com>
2025-02-04 20:43:02 +0530

    Updated Services not pushed so pushing once again three

--- src/components/Forthinverse.jsx
+++ src/components/Forthinverse.jsx
@@ -5,21 +5,21 @@ const Fourthinverse = () => {
   return (
     <div className="bg-transparent h-[20vh] overflow-hidden   ">
       <div className="flex space-x-16 animate-loop-scroll-inverse object-cover">
-        <Image src="/images/EmergencyCare.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
-        <Image src="/images/EmergencyCare.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
-        <Image src="/images/EmergencyCare.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
-        <Image src="/images/EmergencyCare.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
-        <Image src="/images/EmergencyCare.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
-        <Image src="/images/EmergencyCare.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
-        <Image src="/images/EmergencyCare.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
+        <Image src="/images/forthThree.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
+        <Image src="/images/WoundDressing.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
+        <Image src="/images/forthThree.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
+        <Image src="/images/WoundDressing.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
+        <Image src="/images/forthThree.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
+        <Image src="/images/WoundDressing.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
+        <Image src="/images/forthThree.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
         {/* Duplicate images for seamless loop */}
-        <Image src="/images/EmergencyCare.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
-        <Image src="/images/EmergencyCare.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
-        <Image src="/images/EmergencyCare.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
-        <Image src="/images/EmergencyCare.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
-        <Image src="/images/EmergencyCare.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
-        <Image src="/images/EmergencyCare.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
-        <Image src="/images/EmergencyCare.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
+        <Image src="/images/forthThree.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
+        <Image src="/images/WoundDressing.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
+        <Image src="/images/forthThree.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
+        <Image src="/images/WoundDressing.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
+        <Image src="/images/forthThree.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
+        <Image src="/images/WoundDressing.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
+        <Image src="/images/forthThree.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
       </div>
     </div>
   )
--- src/components/Fourthpage.jsx
+++ src/components/Fourthpage.jsx
@@ -10,15 +10,15 @@ const Fourthpage = () => {
         <Image src="/images/forthThree.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
         <Image src="/images/WoundDressing.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
         <Image src="/images/urineService.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
-        <Image src="/images/EmergencyCare.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
-        <Image src="/images/EmergencyCare.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
+        <Image src="/images/forthThree.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
+        <Image src="/images/forthThree.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
         {/* Duplicate images for seamless loop */}
         <Image src="/images/fourthOne.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
         <Image src="/images/fourthTwo.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
         <Image src="/images/forthThree.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
         <Image src="/images/WoundDressing.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
         <Image src="/images/urineService.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
-        <Image src="/images/EmergencyCare.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
+        <Image src="/images/forthThree.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
         <Image src="/images/EmergencyCare.jpg" width={300} height={300} alt="Child" className="w-[150px] h-[150px] md:w-[300px] md:h-[300px]" />
       </div>
     </div>
